e084c76cde2b46ae224bc8b7193ae2e049507123
Anton Borodulin <aborod@softserveinc.com>
2018-05-31 12:52:12 +0300

    add all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent} from './header/header.component';
+import { RecipeBookItemComponent } from './recipe-book/recipe-book-item/recipe-book-item.component';
+import { RecipeBookDetailComponent } from './recipe-book/recipe-book-detail/recipe-book-detail.component';
+import {ShoppingListComponent} from './Shopping/shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from './Shopping/shopping-list-edit/shopping-list-edit.component';
+import {RecipeBookListComponent} from './recipe-book/recipe-book-list/recipe-book-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeBookItemComponent,
+    RecipeBookListComponent,
+    RecipeBookDetailComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule
